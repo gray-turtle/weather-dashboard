a51e73183754cca1403a5b1bbdc0ff06d8d9b2a6
Keith Fike <fkeithr@gmail.com>
2020-06-14 23:13:39 -0600

    connected form to weather display via handlerfunction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
 var currentWeatherEl = document.querySelector("#current-cast");
+var cityInputEl = document.querySelector("#citySearch");
+var userFormEl = document.querySelector("#city-search");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+
+  var cityName = cityInputEl.value.trim();
+
+  if (cityName) {
+    getCurrentWeather(cityName);
+  } else {
+    alert("please enter a valid city name");
+  }
+}
 
 var getCurrentWeather = function(city) {
   var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b25ccab974b7cdb990c6c7ad3e93123b";
@@ -17,17 +31,28 @@ var getCurrentWeather = function(city) {
 }
 
 var displayWeather = function(weather) {
+
+  currentWeatherEl.textContent = "";
+
   var weatherEl = document.createElement("div");
   weatherEl.classList = "list-item flex-row justify-space-between";
+
+  var headerEl = document.createElement("h2");
+  headerEl.innerHTML = "<span>" + cityInputEl.value.trim().toUpperCase() + "</span>";
 
   var mainEl = document.createElement("span");
   mainEl.classList = "flex-row align-center";
 
-  mainEl.innerHTML = "temp: " + Math.floor(((weather.main.temp - 273.15) * (9/5) + 32)) + " degrees fahreneheit";
+  mainEl.innerHTML = 
+  "<i>temp: " + Math.floor(((weather.main.temp - 273.15) * (9/5) + 32)) + " degrees fahreneheit</i></br>"
+  + "<i>Humidity: " + weather.main.humidity + "</i></br>"
+  + "<i>Wind Speed: " + weather.wind.speed + "</i>";
+
+  weatherEl.appendChild(headerEl);
 
   weatherEl.appendChild(mainEl);
 
   currentWeatherEl.appendChild(weatherEl);
 }
 
-getCurrentWeather("london");
+userFormEl.addEventListener("submit", formSubmitHandler);
